e398a51f7fe7717f2fe181624bdcff9b17685b65
shakerscode <ahamedshaker7@gmail..com>
2025-05-14 18:27:16 +0600

    removed unused content

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -213,75 +213,7 @@ const Navbar = () => {
             </motion.div>
           </motion.div>
         )}
-
-        {/* <div
-          className={clsx(
-            "absolute px-4 rounded-none w-5/6 glass bg-black/70 py-4 flex flex-col space-y-4 md:hidden min-h-screen z-50 transition-all duration-500 ease-in-out transform",
-            isOpen
-              ? "translate-x-0 opacity-100 -top-4 -right-5"
-              : "translate-x-full opacity-0 -top-4 -right-96"
-          )}
-        >
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-3">
-              <Search className="h-4 w-4" />
-              <input
-                type="text"
-                name="search"
-                id="search"
-                placeholder="Search"
-                className="outline-none text-sm placeholder:text-sm bg-transparent transition-all ease-in-out duration-300 focus:border-b border-b-beast-white"
-              />
-            </div>
-            <div className="md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="text-white hover:bg-white/10 bg-black/50 p-1.5 rounded-lg"
-              >
-                <X className="h-5 w-5" />
-              </button>
-            </div>
-          </div>
-          {navLinks.map((link) =>
-            link.children ? (
-              <div key={link.name} className="relative group">
-                <span className="cursor-pointer font-medium hover:text-beast-700 dark:hover:text-beast-400 transition-colors">
-                  {link.name}
-                </span>
-                <div className="absolute -left-6 shadow-2xl text-white mt-2 w-48 bg-beast-accent rounded-md opacity-0 group-hover:opacity-100 scale-95 group-hover:scale-100 transform transition duration-200 origin-top z-50">
-                  {link.children.map((sublink) => (
-                    <Link
-                      key={sublink.name}
-                      to={sublink.path}
-                      className="block px-4 py-2 text-base text-white hover:bg-beast-purple-dark hover:text-beast-700"
-                    >
-                      {sublink.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            ) : (
-              <Link
-                key={link.name}
-                to={link.path}
-                className="font-medium hover:text-beast-700 dark:hover:text-beast-400 transition-colors"
-              >
-                {link.name}
-              </Link>
-            )
-          )}
-          <Button
-            onClick={() => navigate("/contact")}
-            size="sm"
-            className={`shadow-glow w-full ${
-              isScrolled
-                ? "bg-white text-beast-purple-dark"
-                : "bg-primary-gradient "
-            }`}
-          >
-            Get In Touch
-          </Button>
-        </div> */}
+ 
       </div>
     </header>
   );
