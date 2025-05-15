1b9d663663bd6f8c3059d35ade6f8bbd7a29760c
yeasinshamimdev <yeasinshamim7@gmail.com>
2025-05-15 13:44:34 +0600

    fix: common loading component erorr

--- src/components/common/CommonLoading.tsx
+++ src/components/common/CommonLoading.tsx
@@ -1,55 +1,29 @@
 import { Variants, motion } from "framer-motion";
 
 const CommonLoading = () => {
-  return function LoadingThreeDotsJumping() {
-    const dotVariants: Variants = {
-      jump: {
-        y: -30,
-        transition: {
-          duration: 0.8,
-          repeat: Infinity,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        },
+  const dotVariants: Variants = {
+    jump: {
+      y: -30,
+      transition: {
+        duration: 0.8,
+        repeat: Infinity,
+        repeatType: "mirror",
+        ease: "easeInOut",
       },
-    };
-
-    return (
-      <motion.div
-        animate="jump"
-        transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
-        className="container"
-      >
-        <motion.div className="dot" variants={dotVariants} />
-        <motion.div className="dot" variants={dotVariants} />
-        <motion.div className="dot" variants={dotVariants} />
-        <StyleSheet />
-      </motion.div>
-    );
+    },
   };
+
+  return (
+    <motion.div
+      animate="jump"
+      transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
+      className="loading-container"
+    >
+      <motion.div className="loading-dot" variants={dotVariants} />
+      <motion.div className="loading-dot" variants={dotVariants} />
+      <motion.div className="loading-dot" variants={dotVariants} />
+    </motion.div>
+  );
 };
 
 export default CommonLoading;
-
-function StyleSheet() {
-  return (
-    <style>
-      {`
-            .container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                gap: 10px;
-            }
-
-            .dot {
-                width: 20px;
-                height: 20px;
-                border-radius: 50%;
-                background-color: #ff0088;
-                will-change: transform;
-            }
-            `}
-    </style>
-  );
-}
