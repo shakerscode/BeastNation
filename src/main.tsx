ae19281db7169afe4896d004798ac0537c8a058f
yeasinshamimdev <yeasinshamim7@gmail.com>
2025-05-20 21:47:45 +0600

    feat: newsletter section

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { ModalProvider } from "./modal/ModalProvider.tsx";
 import { router } from "./routes/router.tsx";
+
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -14,9 +16,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <RouterProvider router={router} />
+        <ModalProvider>
+          <Toaster />
+          <Sonner />
+          <RouterProvider router={router} />
+        </ModalProvider>
       </TooltipProvider>
     </QueryClientProvider>
   </StrictMode>
